30d1ed79095fd3aadd54454d6fca304d3c6a2649
Piotr Petrycki <peterycky@gmail.com>
2023-12-28 20:08:04 +0100

    Updates for ownapi2

--- src/gpt.ts
+++ src/gpt.ts
@@ -1,16 +1,20 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { HumanMessage, SystemMessage } from "langchain/schema";
+import { BaseMessageLike, HumanMessage, SystemMessage } from "langchain/schema";
+
+let messages: BaseMessageLike[] = [];
 
 const model = new ChatOpenAI({
   modelName: "gpt-4",
 });
 
 export const getGPTAnswer = async (message: string) => {
+  messages.push(new HumanMessage(message));
+
   const response = await model.invoke([
     new SystemMessage(
       "You are a very helpful assistant. If multiple questions asked, answer one by one. Answer ultra briefly!"
     ),
-    new HumanMessage(message),
+    ...messages,
   ]);
 
   return {
